19b108844d4cf506fd25d1d8acea961810f8350f
NomanBinBasheer <nomanbinbasheer66626@gmail.com>
2024-06-02 16:06:19 +0500

    added basic structure of app with Next.js TypeScript and TailwindCSS

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-10'>
+      <div className='container'>
+        <h1 className='text-4xl font-bold text-center'>Products CRUD App</h1>
+      </div>
+    </section>
+  )
+}
